5b2178154ae803ee9a44ab4672cbc778ee42745a
anbet <anbet.9900@gamil.com>
2014-08-21 20:51:17 -0700

    Understanding numericals

---
+++ ex3.py
@@ -0,0 +1,8 @@
+print("I will now count my chickens:")
+print("Hens",25+30/6)
+print("Now i will count eggs:")
+print(3+2+1-5+4%2-1/4+6)
+print("Is it true that 3+2<5-7?")
+print(3+2<5-7)
+print("what is 3+2?",3+2)
+print(7.0/4.0++++++7/4)
