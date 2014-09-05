8123948aed6b210209c883006bb6f7379c436441
anbet <anbet.9900@gmail.com>
2014-09-05 08:45:38 +0530

    variales and strings

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age=raw_input("How old are you")
+height=raw_input("What is your height")
+weight=raw_input("What is your Weight")
+
+print "So you are %r old %r tall and you weigh %r."%(age,height,weight)
